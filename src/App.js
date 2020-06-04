ad9a78c333f10598d28f118e8bb0be033f8783ed
hassanabbasi96 <mhassan.abbasi96@gmail.com>
2020-06-04 10:48:21 +0500

    Update App.js
    update title

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello worlds</h1>
+      <h1>Hello from Hassan Abbasi. My first React app.</h1>
     </div>
   );
 }
