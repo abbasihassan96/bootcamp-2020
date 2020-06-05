18d3bf6fcd1851e1fb4cbd4e30dfa3433a4b741d
Hassan Abbasi <mhassan.abbasi96@gmail.com>
2020-06-05 15:56:46 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello from Hassan Abbasi. My first React app.</h1>
+      <h1>Hello from Hassan Abbasi. From MAC.</h1>
     </div>
   );
 }
