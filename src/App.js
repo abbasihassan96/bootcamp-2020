3a9649490d790f6adcfb0a6f8aa3b6429d0b18d9
Hassan Abbasi <mhassan.abbasi96@gmail.com>
2020-06-08 12:10:02 +0500

    Class 3
    Parent child relation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import "./App.css";
+import Dinner from "./dinner";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello from Hassan Abbasi. check again.</h1>
+      <Dinner dishName="Pizza" sweetDish="Icecream" />
     </div>
   );
 }
